25498e609bfee469cde8f303027ac64c03894d56
alexander0354 <alebrekvi@gmail.com>
2023-03-08 20:49:07 +0100

    Lagt til Routing og Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Routes from './components/Resources';
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1>Ressursarkiv</h1>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Route exact path="/" component={Routes} />
+        <Route path="/html" render={() => <Routes category="html" />} />
+        <Route path="/css" render={() => <Routes category="css" />} />
+        <Route
+          path="/javascript"
+          render={() => <Routes category="javascript" />}
+        />
+        <Route path="/react" render={() => <Routes category="react" />} />
+        <Route path="/sanity" render={() => <Routes category="headless-cms" />} />
+      </Layout>
+    </Router>
   );
 }
 
